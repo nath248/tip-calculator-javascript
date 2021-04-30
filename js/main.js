bc56401d5614ae3ee4ae3fb94c9b09a4f13a4479
nath248 <nmherrera20@outlook.com>
2021-04-30 18:54:52 -0400

    Add more styles and javascript variables

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function calculateTip() {
+  var billAmount = document.querySelector("#bill-amount");
+  var serviceQuality = document.querySelector("#service");
+  var numOfPeople = document.querySelector("#bill-split");
+
+  if (billAmount === "" || serviceQuality == 0) {
+    alert("Please enter values");
+    return;
+  }
+
+  }
+}
