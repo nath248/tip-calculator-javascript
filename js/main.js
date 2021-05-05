9923902b51d00e540830537438017997ca9ef90a
nath248 <nmherrera20@outlook.com>
2021-05-05 15:11:17 -0400

    Add alerts to javascript

--- js/main.js
+++ js/main.js
@@ -2,4 +2,16 @@ function calculateTip() {
   var totalBill = document.querySelector("#bill-amount");
   var tipAmount = document.querySelector("#service");
   var numOfPeople = document.querySelector("#people");
+
+
+// Alerts
+  if (totalBill === "") {
+    alert("Please enter a value");
+  };
+
+
+//Calculate tip
+
+  var totalTip.toFixed(2) = (totalBill * tipAmount)/ numOfPeople;
+
 }
