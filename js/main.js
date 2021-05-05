cc62def27beab848d26765365b3aed9af0c29afb
nath248 <nmherrera20@outlook.com>
2021-05-05 14:53:03 -0400

    Add variables

--- js/main.js
+++ js/main.js
@@ -1,12 +1,5 @@
 function calculateTip() {
-  var billAmount = document.querySelector("#bill-amount");
-  var serviceQuality = document.querySelector("#service");
-  var numOfPeople = document.querySelector("#bill-split");
-
-  if (billAmount === "" || serviceQuality == 0) {
-    alert("Please enter values");
-    return;
-  }
-
-  }
+  var totalBill = document.querySelector("#bill-amount");
+  var tipAmount = document.querySelector("#service");
+  var numOfPeople = document.querySelector("#people");
 }
